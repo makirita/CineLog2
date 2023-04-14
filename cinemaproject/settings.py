4023dd6124b1f796fc5a43c77876418a0fb96f09
makirita <arum0079@gmail.com>
2023-04-14 10:23:03 +0900

    host update

--- cinemaproject/settings.py
+++ cinemaproject/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cine-log.com']
 
 
 # Application definition
@@ -142,8 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = '/var/www/gym/cinemaproject/static'
 
 MEDIA_URL = '/media/'
 
