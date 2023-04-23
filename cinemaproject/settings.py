60f1eaf41585c8f0562811fe6bdf90b9cdd616c2
makirita <arum0079@gmail.com>
2023-04-23 14:49:13 +0900

    sec-test6

--- cinemaproject/settings.py
+++ cinemaproject/settings.py
@@ -176,4 +176,4 @@ SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
 
-CSP_STYLE_SRC =("'self'",'7869-7973-8327-01.s3.amazonaws.com/cinelog/css/style.css')
+CSP_STYLE_SRC_ELEM =("'self'",'www.cine-log.com')
