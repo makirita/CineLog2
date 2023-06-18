914ba0e5eaf87b1cb90855ccbfaf1c82fb887a48
makirita <arum0079@gmail.com>
2023-06-18 22:14:54 +0900

    change

--- cinemaproject/settings.py
+++ cinemaproject/settings.py
@@ -113,7 +113,7 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'c-db',
+        'NAME': 'c_db',
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
